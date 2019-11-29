ca4c9630bfc406a79f551e9c3563b36bbc7969d4
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-11-29 20:03:02 +0100

    implement FSK settings

--- modulations.c
+++ modulations.c
@@ -31,9 +31,9 @@
 
 #define INITIAL_FRAME_DELAY_US  (200000) /* 200 ms */
 
-#define TEST_OFDM
-// #define TEST_FSK
-#define TEST_OQPSK
+// #define TEST_OFDM
+#define TEST_FSK
+// #define TEST_OQPSK
 
 typedef struct {
     netopt_t opt;
@@ -172,6 +172,119 @@ static const netopt_list_t oqpsk_chips = {
     },
 };
 #endif /* O-QPSK */
+#ifdef TEST_FSK
+static const netopt_list_t fsk_idx = {
+    .name = "index",
+    .opt  = NETOPT_FSK_MODULATION_INDEX,
+    .data_len = 1,
+    .num_settings = 8,
+    .settings = {
+        {
+            .name = "3/8",
+            .data = 24,
+        },
+        {
+            .name = "1/2",
+            .data = 32,
+        },
+        {
+            .name = "3/4",
+            .data = 48,
+        },
+        {
+            .name = "1",
+            .data = 64,
+        },
+        {
+            .name = "5/4",
+            .data = 80,
+        },
+        {
+            .name = "3/2",
+            .data = 96,
+        },
+        {
+            .name = "7/4",
+            .data = 112,
+        },
+        {
+            .name = "2",
+            .data = 128,
+        },
+    },
+};
+
+static const netopt_list_t fsk_srate = {
+    .name = "srate",
+    .opt  = NETOPT_FSK_SRATE,
+    .data_len = 2,
+    .num_settings = 6,
+    .settings = {
+        {
+            .name = "50 kHz",
+            .data = 50,
+        },
+        {
+            .name = "100 kHz",
+            .data = 100,
+        },
+        {
+            .name = "150 kHz",
+            .data = 150,
+        },
+        {
+            .name = "200 kHz",
+            .data = 200,
+        },
+        {
+            .name = "300 kHz",
+            .data = 300,
+        },
+        {
+            .name = "400 kHz",
+            .data = 400,
+        },
+    },
+};
+
+static const netopt_list_t fsk_mord = {
+    .name = "order",
+    .opt  = NETOPT_FSK_MODULATION_ORDER,
+    .data_len = 1,
+    .num_settings = 2,
+    .settings = {
+        {
+            .name = "2-FSK",
+            .data = 2,
+        },
+        {
+            .name = "4-FSK",
+            .data = 4,
+        },
+    },
+};
+
+static const netopt_list_t fsk_fec = {
+    .name = "FEC",
+    .opt  = NETOPT_FSK_FEC,
+    .data_len = 1,
+    .num_settings = 3,
+    .settings = {
+        {
+            .name = "none",
+            .data = IEEE802154_FEC_NONE,
+        },
+        {
+            .name = "RSC",
+            .data = IEEE802154_FEC_RSC,
+        },
+        {
+            .name = "NRNSC",
+            .data = IEEE802154_FEC_NRNSC,
+        },
+    },
+};
+#endif
 
 static unsigned idx;
 static test_result_t *results[GNRC_NETIF_NUMOF];
@@ -291,7 +404,8 @@ static unsigned _get_FSK_combinations(void)
     static unsigned combinations;
 
     if (combinations == 0) {
-        combinations = 0; /* TODO */
+        combinations = fsk_fec.num_settings * fsk_mord.num_settings
+                     * fsk_srate.num_settings * fsk_idx.num_settings;
     }
 
     return combinations;
@@ -299,14 +413,22 @@ static unsigned _get_FSK_combinations(void)
 
 static int _set_FSK(unsigned setting, bool do_set)
 {
-    for (unsigned i = 0; i < oqpsk_rates.num_settings; ++i) {
-        for (unsigned j = 0; j < oqpsk_chips.num_settings; ++j) {
-            if (setting-- == 0) {
-                printf("FSK ");
-                _set_from_netopt_list(&oqpsk_rates, i, do_set);
-                printf(", ");
-                _set_from_netopt_list(&oqpsk_chips, j, do_set);
-                return 0;
+    for (unsigned i = 0; i < fsk_srate.num_settings; ++i) {
+        for (unsigned j = 0; j < fsk_idx.num_settings; ++j) {
+            for (unsigned k = 0; k < fsk_mord.num_settings; ++k) {
+                for (unsigned l = 0; l < fsk_fec.num_settings; ++l) {
+                    if (setting-- == 0) {
+                        printf("FSK ");
+                        _set_from_netopt_list(&fsk_srate, i, do_set);
+                        printf(", ");
+                        _set_from_netopt_list(&fsk_idx, j, do_set);
+                        printf(", ");
+                        _set_from_netopt_list(&fsk_mord, k, do_set);
+                        printf(", ");
+                        _set_from_netopt_list(&fsk_fec, l, do_set);
+                        return 0;
+                    }
+                }
             }
         }
     }
@@ -330,7 +452,7 @@ static int _set_FSK(unsigned setting, bool do_set)
 
 static unsigned _get_combinations(void)
 {
-    return _get_OFDM_combinations() + _get_OQPSK_combinations();
+    return _get_OFDM_combinations() + _get_OQPSK_combinations() + _get_FSK_combinations();
 }
 
 static int _set(unsigned idx, bool do_set)
